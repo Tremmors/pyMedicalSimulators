9bc93d9dc37ac5869399927a4911b2d6558c1aea
johntrimis <johntrimis@gmail.com>
2011-06-11 18:58:30 -0400

    Fixed some typos, should be functionally identical.

--- GenericASTM.py
+++ GenericASTM.py
@@ -1,3 +1,7 @@
+"""
+        Simulates a generic ASTM device.  Requires the ASTM1394 library.
+
+"""
 import ASTM1394
 import random
 import datetime
@@ -8,7 +12,7 @@ def GetXResults( Count ):
 	MSG.AddLine( 'H|\^&|||^^11223344|||||IDMS||P|1|' + ASTM1394.CurrentDate() )
 	for i in range( 0, Count ):
 		MSG.AddLine( 'P|1||555' )
-		MSG.AddLine( 'O|1||171|^^^A0|||' + ASTM1394.RandomDateInThePastXDays(7) + '|||' )
+		MSG.AddLine( 'O|1||171|^^^|||' + ASTM1394.RandomDateInThePastXDays(7) + '|||' )
 		MSG.AddLine( 'R|1|^^^tHb^M|' + str(random.uniform(2,30)) + '|g/dL||||R' )
 		MSG.AddLine( 'R|2|^^^O2Hb^M|' + str(random.uniform(75,125)) + '|%||||R' )
 		MSG.AddLine( 'R|3|^^^COHb^M|' + str(random.uniform(0,2)) + '|%||||R' )
